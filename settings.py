d6cb194ac4c7b9a8b7be59cbfdfa3b74e34b6b74
Leandro Oniszczuk <loniszczuk@gmail.com>
2012-04-10 02:33:54 -0300

    css

--- settings.py
+++ settings.py
@@ -71,6 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    'static'
 )
 
 # List of finder classes that know how to find static files in
@@ -117,8 +118,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'mofiloterias',
 )
 
