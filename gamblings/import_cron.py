d1031c5abc9a035a944893880601042f0214dbf3
Leandro Oniszczuk <loniszczuk@gmail.com>
2013-05-18 17:02:36 -0300

    add retries to message in queue

--- gamblings/import_cron.py
+++ gamblings/import_cron.py
@@ -25,9 +25,8 @@ def submit_gambling_import_by_date(a_date, rabbit_channel):
     any_gambling_result = GamblingResult.objects.filter(gambling=c.gambling, date=a_date, verified=True)
 
     if not any_gambling_result:
-      message = json.dumps({'date': a_date.isoformat(), 'name': c.gambling.name})
+      message = json.dumps({'date': a_date.isoformat(), 'name': c.gambling.name, 'retries': 0})
 
-      print "publish"
       rabbit_channel.basic_publish(exchange='',
         routing_key='import_gamblings',
         body=message,
